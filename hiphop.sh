dcddb0c43b49dda77295bedb6c5940edc2341ccb
DarthVanger <darthvanger@gmail.com>
2019-03-31 04:28:29 +0300

    Add a snare to the beat

--- hiphop.sh
+++ hiphop.sh
@@ -1,7 +1,9 @@
 repeat=3
 kick="./hiphop/Drums - One Shots/Kicks/Cymatics - Grime Kick 2.wav"
+snare="./hiphop/Drums - One Shots/Snares/Cymatics - Hip-Hop Snare 1 - C.wav"
 sox "$kick" -p pad 0.5 0 pitch 200 |
-sox - -m "$kick" -p pad 0 0.5 pitch -300 |
+sox - -m "$kick" -p pad 0.25 0.5 pitch -300 |
+sox - -m "$snare" -p pad 1 0 |
 sox - output.wav
 
 play output.wav repeat $repeat
