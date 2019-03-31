c6c6f3ceb88a6812fad0ac579348767c6470356c
DarthVanger <darthvanger@gmail.com>
2019-03-31 17:32:22 +0300

    Use the new beat function for all drums

--- hiphop.sh
+++ hiphop.sh
@@ -4,16 +4,6 @@ snare="./hiphop/Drums - One Shots/Snares/Cymatics - Hip-Hop Snare 1 - C.wav"
 perc="./hiphop/Drums - One Shots/Percussion/Cymatics - Low Perc 13.wav"
 
 beat () {
-  file="$1"
-  position="$2"
-  duration=$(soxi -D "$file")
-
-  result=$(echo "$position-$duration" | bc)
-  repeat_number=$(echo "4/$position-1" | bc)
-  echo "pad 0 $result repeat $repeat_number"
-}
-
-beat2 () {
   file="$1"; shift;
   frequency="$1" shift;
   repeats=$(echo "4/$frequency-1" | bc)
@@ -28,19 +18,10 @@ beat2 () {
 rm out/*
 rm output.wav
 
-beat2 "$kick" 0.25 pitch -500
-
-#sox "$kick" out/output1.wav pitch 200 pad 0 0
-#sox "$kick" out/output2.wav pitch 200 pad 1 0
-#sox "$kick" out/output3.wav pitch 200 pad 2 0
-#sox "$kick" out/output4.wav pitch 200 pad 3 0
-#sox "$perc" out/output5.wav pad 0 0
-#sox "$perc" out/output6.wav pad 0.25 0
-#sox "$perc" out/output7.wav pad 0.5 0
-#sox "$perc" out/output8.wav pad 0.75 0
-#sox "$perc" out/output9.wav pad 1 0
-#sox "$kick" out/output10.wav pitch -300 $(beat "$kick" 0.75)
-#sox "$snare" out/output11.wav $(beat "$snare" 1)
+beat "$kick" 0.25
+beat "$perc" 0.5
+beat "$kick" 0.75 pitch -400
+beat "$snare" 1
 
 sox -m out/* output.wav pad 0 4 trim 0 4 vol 20dB
 
