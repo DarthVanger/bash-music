b21d824b4bd4bd509999847d3377390b92220bd3
DarthVanger <darthvanger@gmail.com>
2019-03-31 15:38:03 +0300

    Fix mixing

--- hiphop.sh
+++ hiphop.sh
@@ -3,10 +3,11 @@ kick="./hiphop/Drums - One Shots/Kicks/Cymatics - Grime Kick 2.wav"
 snare="./hiphop/Drums - One Shots/Snares/Cymatics - Hip-Hop Snare 1 - C.wav"
 perc="./hiphop/Drums - One Shots/Percussion/Cymatics - Low Perc 13.wav"
 
-sox "$kick" -p pad 0.5 0 pitch 200 |
-sox - -m "$perc" -p pad 0.1 0 repeat 4 |
-sox - -m "$kick" -p pad 0.25 0.5 pitch -300 |
-sox - -m "$snare" -p pad 1 0 |
-sox - output.wav
+sox "$kick" out/output1.wav pad 0.5 0 pitch 200
+sox "$perc" out/output2.wav pad 0.1 0 repeat 4
+sox "$kick" out/output3.wav pad 0.25 0.5 pitch -300
+sox "$snare" out/output4.wav pad 1 0
 
-play output.wav repeat $repeat
+sox -m out/output*.wav out.wav
+
+play out.wav repeat $repeat
