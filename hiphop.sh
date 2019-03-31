8b40685151a7c67a0782b99250f9a31fa3667c28
DarthVanger <darthvanger@gmail.com>
2019-03-31 20:06:58 +0300

    Add more sounds

--- hiphop.sh
+++ hiphop.sh
@@ -1,7 +1,18 @@
-repeat=3
 kick="./hiphop/Drums - One Shots/Kicks/Cymatics - Grime Kick 2.wav"
 snare="./hiphop/Drums - One Shots/Snares/Cymatics - Hip-Hop Snare 1 - C.wav"
 perc="./hiphop/Drums - One Shots/Percussion/Cymatics - Low Perc 13.wav"
+riser="./hiphop/FX/Risers/Cymatics - Hip-Hop Riser 3 - 150 BPM E.wav"
+downlifter="./hiphop/FX/Downlifters/Cymatics - Hip-Hop Downlifter 1 - 140 BPM C.wav"
+impact="./hiphop/FX/Impacts/Cymatics - Halloween Ultimate Impact 13.wav"
+banging808="./hiphop/808s/Cymatics - Bangin 808 2 - C.wav"
+grime808="./hiphop/808s/Cymatics - Grime 808 25 - G.wav"
+hiphop808="./hiphop/808s/Cymatics - Hip-Hop 808 10 - B.wav"
+trap808="./hiphop/808s/Cymatics - Trap 808 2 - D.wav"
+booming808="./hiphop/808s/Cymatics - Boomin 808 1 - C.wav"
+vocal_chant="./hiphop/Vocal Chants/Cymatics - Hip-Hop Vocal Chant 1 - Crowd Breath.wav"
+vocal_chant_titan="./hiphop/Vocal Chants/Cymatics - Titan Vocal Chant - Flex.wav"
+melody_loop="./hiphop/Melody Loops/Cymatics - Hip-Hop Melody Loop 10 - 128 BPM C# Min.wav"
+dubstep="dubstep/Synths - One Shots/Cymatics - Dubstep Growl 5 - F.wav"
 
 beat () {
   file="$1"; shift;
@@ -15,9 +26,17 @@ beat () {
   done
 }
 
+one () {
+  file="$1"; shift;
+  random=$(shuf -i0-1000 -n1)
+  echo "SOxing: $file out/output-$random.wav $@"
+  sox "$file" out/output-$random.wav $@
+}
+
 rm out/*
 rm output.wav
 
+#one "$riser"
 beat "$kick" 0.25
 beat "$perc" 0.5
 beat "$kick" 0.75 pitch -400
