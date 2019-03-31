db8a715779bc3469708cc1787bb89444f0c474e9
DarthVanger <darthvanger@gmail.com>
2019-03-31 17:26:25 +0300

    Implement function for beat using loop

--- hiphop.sh
+++ hiphop.sh
@@ -13,14 +13,35 @@ beat () {
   echo "pad 0 $result repeat $repeat_number"
 }
 
+beat2 () {
+  file="$1"
+  frequency="$2"
+  repeats=$(echo "4/$frequency-1" | bc)
+  for i in $(seq 0 $repeats)
+  do
+    random=$(shuf -i0-1000 -n1)
+    pad=$(echo "$i*$frequency" | bc)
+    sox "$file" out/output-$i-$random.wav pad $pad
+  done
+}
+
 rm out/*
 rm output.wav
 
-sox "$kick" out/output1.wav pitch 200 $(beat "$kick" 0.25)
-sox "$perc" out/output2.wav $(beat "$perc" 0.5)
-sox "$kick" out/output3.wav pitch -300 $(beat "$kick" 0.75)
-sox "$snare" out/output4.wav $(beat "$snare" 1)
+beat2 "$kick" 0.1
 
-sox -m out/* output.wav pad 0 4 trim 0 4
+#sox "$kick" out/output1.wav pitch 200 pad 0 0
+#sox "$kick" out/output2.wav pitch 200 pad 1 0
+#sox "$kick" out/output3.wav pitch 200 pad 2 0
+#sox "$kick" out/output4.wav pitch 200 pad 3 0
+#sox "$perc" out/output5.wav pad 0 0
+#sox "$perc" out/output6.wav pad 0.25 0
+#sox "$perc" out/output7.wav pad 0.5 0
+#sox "$perc" out/output8.wav pad 0.75 0
+#sox "$perc" out/output9.wav pad 1 0
+#sox "$kick" out/output10.wav pitch -300 $(beat "$kick" 0.75)
+#sox "$snare" out/output11.wav $(beat "$snare" 1)
+
+sox -m out/* output.wav pad 0 4 trim 0 4 vol 20dB
 
 play output.wav repeat $repeat
