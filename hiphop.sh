ef5ab47704fa860d33510afe86f0fbe22ce8b938
DarthVanger <darthvanger@gmail.com>
2019-03-31 04:32:09 +0300

    Add percussion

--- hiphop.sh
+++ hiphop.sh
@@ -1,7 +1,10 @@
 repeat=3
 kick="./hiphop/Drums - One Shots/Kicks/Cymatics - Grime Kick 2.wav"
 snare="./hiphop/Drums - One Shots/Snares/Cymatics - Hip-Hop Snare 1 - C.wav"
+perc="./hiphop/Drums - One Shots/Percussion/Cymatics - Low Perc 13.wav"
+
 sox "$kick" -p pad 0.5 0 pitch 200 |
+sox - -m "$perc" -p pad 0.1 0 repeat 4 |
 sox - -m "$kick" -p pad 0.25 0.5 pitch -300 |
 sox - -m "$snare" -p pad 1 0 |
 sox - output.wav
