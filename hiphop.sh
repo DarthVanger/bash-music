0b2b0ab80eacf9516ec6c81be8351c34f401d70e
DarthVanger <darthvanger@gmail.com>
2019-04-05 00:52:42 +0300

    Adjust hiphop_loop length to 16 sec

--- hiphop.sh
+++ hiphop.sh
@@ -1,3 +1,21 @@
+beat () {
+  file="$1"; shift;
+  frequency="$1" shift;
+  repeats=$(echo "4/$frequency-1" | bc)
+  for i in $(seq 0 $repeats)
+  do
+    random=$(shuf -i0-1000 -n1)
+    pad=$(echo "$i*$frequency" | bc)
+    sox "$file" out/output-$i-$random.wav pad $pad $@
+  done
+}
+
+once () {
+  file="$1"; shift;
+  random=$(shuf -i0-1000 -n1)
+  sox "$file" out/output-$random.wav $@
+}
+
 kick="./hiphop/Drums - One Shots/Kicks/Cymatics - Grime Kick 2.wav"
 snare="./hiphop/Drums - One Shots/Snares/Cymatics - Hip-Hop Snare 1 - C.wav"
 perc="./hiphop/Drums - One Shots/Percussion/Cymatics - Low Perc 13.wav"
@@ -15,36 +33,18 @@ melody_loop="./hiphop/Melody Loops/Cymatics - Hip-Hop Melody Loop 10 - 128 BPM C
 dubstep="dubstep/Synths - One Shots/Cymatics - Dubstep Growl 5 - F.wav"
 synth1="./synth/synth1.wav"
 synth2="./synth/synth2.wav"
-
-beat () {
-  file="$1"; shift;
-  frequency="$1" shift;
-  repeats=$(echo "4/$frequency-1" | bc)
-  for i in $(seq 0 $repeats)
-  do
-    random=$(shuf -i0-1000 -n1)
-    pad=$(echo "$i*$frequency" | bc)
-    sox "$file" out/output-$i-$random.wav pad $pad $@
-  done
-}
-
-one () {
-  file="$1"; shift;
-  random=$(shuf -i0-1000 -n1)
-  echo "SOxing: $file out/output-$random.wav $@"
-  sox "$file" out/output-$random.wav $@
-}
+hiphop_loop="./hiphop/Melody Loops/Cymatics - Hip-Hop Melody Loop 10 - 128 BPM C# Min.wav"
 
 rm out/*
 rm output.wav
 
-one "$synth1"
-one "$synth2" pad 0.5
-#beat "$kick" 0.25
+once "$hiphop_loop" tempo $(echo "15/16" | bc -l)
+once "$kick" pad 4
 #beat "$perc" 0.5
 #beat "$kick" 0.75 pitch -400
 #beat "$snare" 1
 
-sox -m out/* output.wav pad 0 4 trim 0 4
+sox -m out/* output.wav pad 0 16 trim 0 16
 
 play output.wav repeat $repeat
+
